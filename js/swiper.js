b22b1e9a61435f8c0ce33cb260ad9c5e6f73fd04
SakiCode <saki91ns@gmail.com>
2021-03-04 15:07:04 +0100

    responsive version

--- js/swiper.js
+++ js/swiper.js
@@ -1,9 +1,22 @@
 const swiper = new Swiper(".swiper-container", {
-	slidesPerView: 3,
-	spaceBetween: 30,
-	slidesPerGroup: 3,
+	slidesPerView: 1,
+	spaceBetween: 10,
+	breakpoints: {
+		// when window width is >= 1200px
+		650: {
+			slidesPerView: 2,
+			spaceBetween: 20,
+			slidesPerGroup: 2,
+		},
+		// when window width is >= 1200px
+		1100: {
+			slidesPerView: 3,
+			spaceBetween: 30,
+			slidesPerGroup: 3,
+		},
+	},
 	navigation: {
 		nextEl: ".swiper-button-next",
 		prevEl: ".swiper-button-prev",
-	},
+	}
 });
